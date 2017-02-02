460994ea2fd6e33fc49b6809b550a8ebe10f2c9f
FieryEcho <jasonrod651@gmail.com>
2017-02-01 21:21:42 -0500

    Just fixed the sleep method

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Autonomous/Robot_Methods.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Autonomous/Robot_Methods.java
@@ -1,21 +1,26 @@
 package org.firstinspires.ftc.teamcode.Autonomous;
 
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+
 import org.firstinspires.ftc.teamcode.HardwareFireWiresBot;
+
 /**
  * Created by 17rodrigues on 2/1/17.
  */
 
 public class Robot_Methods {
-    public static void waitSeconds(double seconds){
-        long milliseconds = (long)(seconds *1000);
-        waitSeconds(milliseconds);
+    public static void waitSeconds(double seconds) {
+        long milliseconds = (long) (seconds * 1000);
+        try {
+            Thread.sleep(milliseconds);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 
     /**
-     *
-     * @param robot The robot to act on
-     * @param power The power for both motors
+     * @param robot   The robot to act on
+     * @param power   The power for both motors
      * @param seconds The number of seconds to drive
      */
     public static void driveForSecondsAtPower(HardwareFireWiresBot robot, double power, double seconds) {
@@ -23,7 +28,8 @@ public class Robot_Methods {
         waitSeconds(seconds);
         robot.drive(0, 0);
     }
-    public static void shootOneBall(HardwareFireWiresBot robot, double shooterPower){
+
+    public static void shootOneBall(HardwareFireWiresBot robot, double shooterPower) {
         /* Fire! */
         robot.leftShooter.setPower(shooterPower);
         robot.rightShooter.setPower(shooterPower);
